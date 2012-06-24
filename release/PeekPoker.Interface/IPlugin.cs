960ed9fa4d48ace3bd9b74c57f5c92e815993965
pureIsometric@gmail.com <pureIsometric@gmail.com@d7f8779e-b4c0-4aed-5867-c6d0af2daefe>
2012-06-24 11:33:38 +0000

    Initial Upload

---
+++ release/PeekPoker.Interface/IPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PeekPoker.Interface
+{
+    //======================================================//
+    // Interface that would allow other applications/tools  //
+    // to be plugged into the peekpoker application.        //
+    // =====================================================//
+    public interface IPlugin
+    {
+        #region plugin description
+        string Name { get; }        // Plugin Name used for the Plugin Browser and Downloader/Updater
+        string Description { get; } // Plugin Description for the About Window
+        string Author { get; }      // Plugin Author(s)
+        Version Version { get; }    // Plugin Version
+        #endregion
+
+
+    }
+}
