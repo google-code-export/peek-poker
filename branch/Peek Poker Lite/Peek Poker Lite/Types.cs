41973d48e08879a03337b84948ce344b3dc3c434
pureIsometric <pureIsometric@d7f8779e-b4c0-4aed-5867-c6d0af2daefe>
2012-11-27 23:13:30 +0000

    Deleted for renewal

--- branch/Peek Poker Lite/Peek Poker Lite/Types.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Peek_Poker_Lite
-{
-    public class SearchResults
-    {
-        public string Id { get; set; }
-        public string Offset { get; set; }
-        public string Value { get; set; }
-    }
-}
