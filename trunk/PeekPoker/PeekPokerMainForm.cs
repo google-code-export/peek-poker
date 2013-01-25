d4fd48519e9a063d59fc2f5d6c583e6677c99480
mhutchinson221@googlemail.com <mhutchinson221@googlemail.com@d7f8779e-b4c0-4aed-5867-c6d0af2daefe>
2013-01-25 23:14:40 +0000

    Switched IP storage over to registry at DefaultIP in xdk folder, code resharp'd [code cleanup]

--- trunk/PeekPoker/PeekPokerMainForm.cs
+++ trunk/PeekPoker/PeekPokerMainForm.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 using Microsoft.Win32;
 using PeekPoker.Interface;
 using PeekPoker.Plugin;
-using Application = System.Windows.Forms.Application;
-using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace PeekPoker
 {
@@ -17,10 +16,10 @@ namespace PeekPoker
     {
         #region global varibales
 
-        private PluginService _pluginService;
         private ListViewItem _listviewItem;
-        private RealTimeMemory _rtm;//DLL is now in the Important File Folder
-        private readonly string _filepath = (Application.StartupPath + "\\XboxIP.txt"); //For IP address loading - 8Ball
+        private PluginService _pluginService;
+        private RealTimeMemory _rtm; //DLL is now in the Important File Folder
+
         #endregion
 
         public PeekPokerMainForm()
@@ -30,34 +29,28 @@ namespace PeekPoker
             //SetLogText("Please make sure you have the xbdm xbox 360 plug-in loaded.");
             //SetLogText("All the information provided in this application is for educational purposes only. Neither the application nor host are in any way responsible for any misuse of the information.");
             LoadPlugins();
-         
         }
+
         private void MainFormFormClosing(object sender, FormClosingEventArgs e)
         {
-            Process.GetCurrentProcess().Kill();//Immediately stop the process
+            Process.GetCurrentProcess().Kill(); //Immediately stop the process
         }
+
         private void Form1Load(Object sender, EventArgs e)
         {
             try
             {
-                //feature suggested by Fairchild
-                var ipRegex = new Regex(@"^(([01]?\d\d?|2[0-4]\d|25[0-5])\.){3}([01]?\d\d?|25[0-5]|2[0-4]\d)$");
-                    //IP Check for XDK Name
-                var xboxname =
+                ipAddressTextBox.Text =
                     (string)
-                    Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\XenonSDK", "XboxName", "NotFound");
-                    //feature suggested by fairchild -Introduced regex to accomodate the possiblity of a name instead of ip, which is very possible.
-                var validIp = ipRegex.Match(xboxname); // For regex to check if there's a match
-                if (validIp.Success)
-                    ipAddressTextBox.Text = xboxname; // If the registry contains a valid IP, send to textbox.
-                if (File.Exists(_filepath))
-                    ipAddressTextBox.Text = File.ReadAllText(_filepath);
-
+                    Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\XenonSDK", "DefaultIP", "0.0.0.0");
             }
-            catch (Exception){}
-        }      
-       
+            catch (Exception)
+            {
+            }
+        }
+
         #region button clicks
+
         private void AboutToolStripMenuItem1Click(object sender, EventArgs e)
         {
             var stringBuilder = new StringBuilder();
@@ -65,15 +58,19 @@ namespace PeekPoker
             stringBuilder.AppendLine(string.Format("================By===================="));
             stringBuilder.AppendLine(string.Format("Cybersam, 8Ball, PureIso & Jappi88"));
             stringBuilder.AppendLine(string.Format("=============Special Thanks To========="));
-            stringBuilder.AppendLine(string.Format("optantic (tester), Mojobojo (codes), Natelx (xbdm), Be.Windows.Forms.HexBox.dll"));
+            stringBuilder.AppendLine(
+                string.Format("optantic (tester), Mojobojo (codes), Natelx (xbdm), Be.Windows.Forms.HexBox.dll"));
             stringBuilder.AppendLine(string.Format("Fairchild (codes), actualmanx (tester), ioritree (tester), 360Haven"));
-            ShowMessageBox(stringBuilder.ToString(), string.Format("Peek Poker"), MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ShowMessageBox(stringBuilder.ToString(), string.Format("Peek Poker"), MessageBoxButtons.OK,
+                           MessageBoxIcon.Information);
         }
+
         //When you click on the connect button
         private void ConnectButtonClick(object sender, EventArgs e)
         {
             ThreadPool.QueueUserWorkItem(Connect);
         }
+
         //When you click on the www.360haven.com
         private void ToolStripStatusLabel2Click(object sender, EventArgs e)
         {
@@ -82,8 +79,9 @@ namespace PeekPoker
 
         private void peekNpokeButton_Click(object sender, EventArgs e)
         {
-            PeekNPoke form = displayOutsideParentBox.Checked ?
-                                 new PeekNPoke(_rtm) : new PeekNPoke(_rtm) { MdiParent = this };
+            PeekNPoke form = displayOutsideParentBox.Checked
+                                 ? new PeekNPoke(_rtm)
+                                 : new PeekNPoke(_rtm) {MdiParent = this};
             form.ShowMessageBox += ShowMessageBox;
             form.UpdateProgressbar += UpdateProgressbar;
             form.EnableControl += EnableControl;
@@ -94,8 +92,9 @@ namespace PeekPoker
 
         private void dumpButton_Click(object sender, EventArgs e)
         {
-            Dump form = displayOutsideParentBox.Checked ?
-                new Dump(_rtm) : new Dump(_rtm) { MdiParent = this };
+            Dump form = displayOutsideParentBox.Checked
+                            ? new Dump(_rtm)
+                            : new Dump(_rtm) {MdiParent = this};
             form.ShowMessageBox += ShowMessageBox;
             form.EnableControl += EnableControl;
             form.GetTextBoxText += GetTextBoxText;
@@ -105,8 +104,9 @@ namespace PeekPoker
 
         private void SearchButtonClick(object sender, EventArgs e)
         {
-            Search form = displayOutsideParentBox.Checked ?
-                new Search(_rtm) : new Search(_rtm) { MdiParent = this };
+            Search form = displayOutsideParentBox.Checked
+                              ? new Search(_rtm)
+                              : new Search(_rtm) {MdiParent = this};
             form.ShowMessageBox += ShowMessageBox;
             form.EnableControl += EnableControl;
             form.GetTextBoxText += GetTextBoxText;
@@ -116,26 +116,30 @@ namespace PeekPoker
 
         private void converterButton_Click(object sender, EventArgs e)
         {
-            Converter form = displayOutsideParentBox.Checked ?
-                new Converter() : new Converter { MdiParent = this };
+            Converter form = displayOutsideParentBox.Checked
+                                 ? new Converter()
+                                 : new Converter {MdiParent = this};
             form.ShowMessageBox += ShowMessageBox;
             form.Show();
         }
 
         private void pluginInfoButton_Click(object sender, EventArgs e)
         {
-            PluginInfo form = displayOutsideParentBox.Checked ?
-                new PluginInfo(_listviewItem) : new PluginInfo(_listviewItem) { MdiParent = this };
+            PluginInfo form = displayOutsideParentBox.Checked
+                                  ? new PluginInfo(_listviewItem)
+                                  : new PluginInfo(_listviewItem) {MdiParent = this};
             form.Show();
         }
+
         #endregion
 
         #region Functions
+
         private void LoadPlugins()
         {
             try
             {
-                var pathToPlugins = AppDomain.CurrentDomain.BaseDirectory + "Plugins";
+                string pathToPlugins = AppDomain.CurrentDomain.BaseDirectory + "Plugins";
                 if (!(Directory.Exists(pathToPlugins))) Directory.CreateDirectory(pathToPlugins);
 
                 _pluginService = new PluginService(pathToPlugins);
@@ -144,13 +148,13 @@ namespace PeekPoker
                 {
                     EnableControl(pluginInfoButton, true);
                     var item = new ToolStripMenuItem
-                        {
-                            Name = pluginData.ApplicationName,
-                            Tag = pluginData.ApplicationName,
-                            Text = pluginData.ApplicationName,
-                            Image = pluginData.Icon.ToBitmap(),
-                            Size = new System.Drawing.Size(170, 22)
-                        };
+                                   {
+                                       Name = pluginData.ApplicationName,
+                                       Tag = pluginData.ApplicationName,
+                                       Text = pluginData.ApplicationName,
+                                       Image = pluginData.Icon.ToBitmap(),
+                                       Size = new Size(170, 22)
+                                   };
                     item.Click += PluginClickEventHandler; //Event handler if you click on the cheat code
                     pluginsToolStripMenuItem.DropDownItems.Add(item);
 
@@ -163,15 +167,16 @@ namespace PeekPoker
             }
             catch (Exception e)
             {
-				ShowMessageBox(e.Message,"Peek Poker", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                ShowMessageBox(e.Message, "Peek Poker", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
+
         //The click handler for the plugins
         private void PluginClickEventHandler(object sender, EventArgs e)
         {
             try
             {
-                var item = (ToolStripMenuItem)sender;       // get the menu item
+                var item = (ToolStripMenuItem) sender; // get the menu item
                 foreach (AbstractPlugin plugin in _pluginService.PluginDatas)
                 {
                     if (plugin.ApplicationName == item.Name)
@@ -186,17 +191,18 @@ namespace PeekPoker
             {
                 MessageBox.Show(ex.Message);
             }
-            
         }
+
         #endregion
 
         #region Thread Functions
+
         private void Connect(object a)
         {
             try
             {
                 string ipAddress = GetTextBoxText(ipAddressTextBox);
-                _rtm = new RealTimeMemory(ipAddress, 0, 0);//initialize real time memory
+                _rtm = new RealTimeMemory(ipAddress, 0, 0); //initialize real time memory
                 _rtm.ReportProgress += UpdateProgressbar;
 
                 if (!_rtm.Connect())
@@ -206,76 +212,79 @@ namespace PeekPoker
                 EnableControl(mainGroupBox, true);
 
                 SetToolStripLabel("Connected!");
+                Registry.SetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\XenonSDK", "DefaultIP",
+                                  ipAddress.ToString(CultureInfo.InvariantCulture)); //Store IP in registry
                 ShowMessageBox("Connected!", "Peek Poker", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
-                if (!File.Exists(_filepath)) File.Create(_filepath).Dispose(); //Create the file if it doesn't exist
-                using (StreamWriter objWriter = new StreamWriter(_filepath))
-                {
-                    //Writer Declaration
-                    objWriter.Write(ipAddress); //Writes IP address to text file
-                    objWriter.Close(); //Close Writer
-                    SetTextBoxText(connectButton, "Re-Connect");
-                }
+                SetTextBoxText(connectButton, "Re-Connect");
             }
             catch (Exception ex)
             {
                 ShowMessageBox(ex.Message, String.Format("Peek Poker"), MessageBoxButtons.OK,
-                                MessageBoxIcon.Error);
+                               MessageBoxIcon.Error);
             }
         }
+
         #endregion
 
         #region safeThreadingProperties
+
         //Getter Methods
         private string GetTextBoxText(Control control)
         {
             //recursion
-            var returnVal = "";
-            if (control.InvokeRequired) control.Invoke((MethodInvoker)
-                  delegate { returnVal = GetTextBoxText(control); });
+            string returnVal = "";
+            if (control.InvokeRequired)
+                control.Invoke((MethodInvoker)
+                               delegate { returnVal = GetTextBoxText(control); });
             else
                 return control.Text;
             return returnVal;
         }
+
         private void EnableControl(Control control, bool value)
         {
             if (control.InvokeRequired)
-                control.Invoke((MethodInvoker)(() => EnableControl(control, value)));
+                control.Invoke((MethodInvoker) (() => EnableControl(control, value)));
             else
                 control.Enabled = value;
         }
+
         //Setter Methods
         private void SetToolStripLabel(string value)
         {
             if (InvokeRequired)
-                Invoke((MethodInvoker)(() => SetToolStripLabel(value)));
+                Invoke((MethodInvoker) (() => SetToolStripLabel(value)));
             else
             {
                 statusStripLabel.Text = value;
             }
         }
+
         private void SetTextBoxText(Control control, string value)
         {
             if (control.InvokeRequired)
-                Invoke((MethodInvoker)(() => SetTextBoxText(control, value)));
+                Invoke((MethodInvoker) (() => SetTextBoxText(control, value)));
             else
             {
                 control.Text = value;
             }
         }
+
         private void ShowMessageBox(string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
         {
             //Using lambda express - I believe its slower - Just an example
             if (InvokeRequired)
-                Invoke((MethodInvoker)(() => ShowMessageBox(text, caption, buttons, icon)));
+                Invoke((MethodInvoker) (() => ShowMessageBox(text, caption, buttons, icon)));
             else MessageBox.Show(this, text, caption, buttons, icon);
         }
+
         //Progress bar Delegates
         private void UpdateProgressbar(int min, int max, int value, string text = "Idle")
         {
             if (statusStrip1.InvokeRequired)
             {
-                statusStrip1.Invoke((MethodInvoker)(() => UpdateProgressbar(min, max, value, text)));
+                statusStrip1.Invoke((MethodInvoker) (() => UpdateProgressbar(min, max, value, text)));
             }
             else
             {
@@ -287,9 +296,8 @@ namespace PeekPoker
                 }
                 statusStripLabel.Text = text;
             }
-
-
         }
+
         #endregion
     }
 }
