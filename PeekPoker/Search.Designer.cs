f740dd639d04ae67cf30022830b09f65da2c4f4c
pureIsometric <pureIsometric@d7f8779e-b4c0-4aed-5867-c6d0af2daefe>
2013-01-26 11:08:49 +0000

    Copy and paste from search results - DONE Stop search close rtm _ TODO
    remove changed values - search refresh
    Add New Refresh Functions (If greater, less than, equals) - DONE (NOT TEsted)
    
    poke takes too long - Apparently poke is poking all the values instead of the values you change (You notice when poking 1 byte over 0xFFFFFF length)

--- PeekPoker/Search.Designer.cs
+++ PeekPoker/Search.Designer.cs
@@ -40,18 +40,27 @@
             this.label6 = new System.Windows.Forms.Label();
             this.searchRangeValueTextBox = new System.Windows.Forms.TextBox();
             this.lengthRangeAddressTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.defaultRadioButton = new System.Windows.Forms.RadioButton();
+            this.ifEqualsRadioButton = new System.Windows.Forms.RadioButton();
+            this.ifGreaterThanRadioButton = new System.Windows.Forms.RadioButton();
+            this.ifLessThanRadioButton = new System.Windows.Forms.RadioButton();
+            this.ifChangeRadioButton = new System.Windows.Forms.RadioButton();
+            this.newValueTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.totalTextBoxText = new System.Windows.Forms.TextBox();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).BeginInit();
             this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.resultGrid);
-            this.groupBox2.Controls.Add(this.resultRefreshButton);
-            this.groupBox2.Location = new System.Drawing.Point(12, 132);
+            this.groupBox2.Location = new System.Drawing.Point(12, 110);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(340, 200);
+            this.groupBox2.Size = new System.Drawing.Size(340, 197);
             this.groupBox2.TabIndex = 21;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Result/s";
@@ -64,21 +73,22 @@
             this.resultGrid.BackgroundColor = System.Drawing.SystemColors.Control;
             this.resultGrid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.resultGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.resultGrid.Location = new System.Drawing.Point(6, 18);
+            this.resultGrid.Location = new System.Drawing.Point(6, 12);
             this.resultGrid.MultiSelect = false;
             this.resultGrid.Name = "resultGrid";
             this.resultGrid.ReadOnly = true;
             this.resultGrid.RowHeadersVisible = false;
             this.resultGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.resultGrid.Size = new System.Drawing.Size(328, 138);
+            this.resultGrid.Size = new System.Drawing.Size(328, 173);
             this.resultGrid.TabIndex = 14;
             this.resultGrid.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.ResultGridCellValueChanged);
+            this.resultGrid.KeyDown += new System.Windows.Forms.KeyEventHandler(this.resultGrid_KeyDown);
             // 
             // resultRefreshButton
             // 
-            this.resultRefreshButton.Location = new System.Drawing.Point(139, 164);
+            this.resultRefreshButton.Location = new System.Drawing.Point(48, 157);
             this.resultRefreshButton.Name = "resultRefreshButton";
-            this.resultRefreshButton.Size = new System.Drawing.Size(76, 30);
+            this.resultRefreshButton.Size = new System.Drawing.Size(76, 28);
             this.resultRefreshButton.TabIndex = 13;
             this.resultRefreshButton.Text = "Refresh";
             this.resultRefreshButton.UseVisualStyleBackColor = true;
@@ -86,6 +96,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.totalTextBoxText);
             this.groupBox1.Controls.Add(this.label12);
             this.groupBox1.Controls.Add(this.stopSearchButton);
             this.groupBox1.Controls.Add(this.searchRangeButton);
@@ -96,7 +108,7 @@
             this.groupBox1.Controls.Add(this.lengthRangeAddressTextBox);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(340, 114);
+            this.groupBox1.Size = new System.Drawing.Size(539, 92);
             this.groupBox1.TabIndex = 20;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Search Range Selection Options";
@@ -104,7 +116,7 @@
             // label12
             // 
             this.label12.AutoSize = true;
-            this.label12.Location = new System.Drawing.Point(202, 21);
+            this.label12.Location = new System.Drawing.Point(165, 34);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(43, 13);
             this.label12.TabIndex = 19;
@@ -113,9 +125,9 @@
             // stopSearchButton
             // 
             this.stopSearchButton.Enabled = false;
-            this.stopSearchButton.Location = new System.Drawing.Point(232, 74);
+            this.stopSearchButton.Location = new System.Drawing.Point(432, 51);
             this.stopSearchButton.Name = "stopSearchButton";
-            this.stopSearchButton.Size = new System.Drawing.Size(94, 32);
+            this.stopSearchButton.Size = new System.Drawing.Size(94, 34);
             this.stopSearchButton.TabIndex = 18;
             this.stopSearchButton.Text = "Stop Search";
             this.stopSearchButton.UseVisualStyleBackColor = true;
@@ -123,9 +135,9 @@
             // 
             // searchRangeButton
             // 
-            this.searchRangeButton.Location = new System.Drawing.Point(75, 74);
+            this.searchRangeButton.Location = new System.Drawing.Point(432, 13);
             this.searchRangeButton.Name = "searchRangeButton";
-            this.searchRangeButton.Size = new System.Drawing.Size(140, 32);
+            this.searchRangeButton.Size = new System.Drawing.Size(94, 34);
             this.searchRangeButton.TabIndex = 9;
             this.searchRangeButton.Text = "Search Hex Value";
             this.searchRangeButton.UseVisualStyleBackColor = true;
@@ -135,7 +147,7 @@
             // 
             this.startRangeAddressTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.startRangeAddressTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.startRangeAddressTextBox.Location = new System.Drawing.Point(75, 22);
+            this.startRangeAddressTextBox.Location = new System.Drawing.Point(78, 31);
             this.startRangeAddressTextBox.Name = "startRangeAddressTextBox";
             this.startRangeAddressTextBox.Size = new System.Drawing.Size(75, 20);
             this.startRangeAddressTextBox.TabIndex = 5;
@@ -145,7 +157,7 @@
             // label13
             // 
             this.label13.AutoSize = true;
-            this.label13.Location = new System.Drawing.Point(7, 51);
+            this.label13.Location = new System.Drawing.Point(7, 62);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(62, 13);
             this.label13.TabIndex = 10;
@@ -154,7 +166,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(4, 25);
+            this.label6.Location = new System.Drawing.Point(7, 34);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(63, 13);
             this.label6.TabIndex = 0;
@@ -164,9 +176,9 @@
             // 
             this.searchRangeValueTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.searchRangeValueTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.searchRangeValueTextBox.Location = new System.Drawing.Point(75, 48);
+            this.searchRangeValueTextBox.Location = new System.Drawing.Point(75, 59);
             this.searchRangeValueTextBox.Name = "searchRangeValueTextBox";
-            this.searchRangeValueTextBox.Size = new System.Drawing.Size(251, 20);
+            this.searchRangeValueTextBox.Size = new System.Drawing.Size(344, 20);
             this.searchRangeValueTextBox.TabIndex = 8;
             this.searchRangeValueTextBox.Text = "FF00FF00FF00";
             this.searchRangeValueTextBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SearchRangeValueTextBoxKeyUp);
@@ -176,18 +188,117 @@
             // 
             this.lengthRangeAddressTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
             this.lengthRangeAddressTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
-            this.lengthRangeAddressTextBox.Location = new System.Drawing.Point(251, 18);
+            this.lengthRangeAddressTextBox.Location = new System.Drawing.Point(214, 31);
             this.lengthRangeAddressTextBox.Name = "lengthRangeAddressTextBox";
             this.lengthRangeAddressTextBox.Size = new System.Drawing.Size(75, 20);
             this.lengthRangeAddressTextBox.TabIndex = 7;
             this.lengthRangeAddressTextBox.Text = "0xFF";
             this.lengthRangeAddressTextBox.Leave += new System.EventHandler(this.FixTheAddresses);
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.newValueTextBox);
+            this.groupBox3.Controls.Add(this.resultRefreshButton);
+            this.groupBox3.Controls.Add(this.ifChangeRadioButton);
+            this.groupBox3.Controls.Add(this.ifLessThanRadioButton);
+            this.groupBox3.Controls.Add(this.ifGreaterThanRadioButton);
+            this.groupBox3.Controls.Add(this.ifEqualsRadioButton);
+            this.groupBox3.Controls.Add(this.defaultRadioButton);
+            this.groupBox3.Location = new System.Drawing.Point(359, 110);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(192, 197);
+            this.groupBox3.TabIndex = 22;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Remove If Not";
+            // 
+            // defaultRadioButton
+            // 
+            this.defaultRadioButton.AutoSize = true;
+            this.defaultRadioButton.Checked = true;
+            this.defaultRadioButton.Location = new System.Drawing.Point(6, 16);
+            this.defaultRadioButton.Name = "defaultRadioButton";
+            this.defaultRadioButton.Size = new System.Drawing.Size(143, 17);
+            this.defaultRadioButton.TabIndex = 0;
+            this.defaultRadioButton.TabStop = true;
+            this.defaultRadioButton.Text = "Default (Original Refresh)";
+            this.defaultRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ifEqualsRadioButton
+            // 
+            this.ifEqualsRadioButton.AutoSize = true;
+            this.ifEqualsRadioButton.Location = new System.Drawing.Point(6, 39);
+            this.ifEqualsRadioButton.Name = "ifEqualsRadioButton";
+            this.ifEqualsRadioButton.Size = new System.Drawing.Size(100, 17);
+            this.ifEqualsRadioButton.TabIndex = 1;
+            this.ifEqualsRadioButton.Text = "If Equals (Keep)";
+            this.ifEqualsRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ifGreaterThanRadioButton
+            // 
+            this.ifGreaterThanRadioButton.AutoSize = true;
+            this.ifGreaterThanRadioButton.Location = new System.Drawing.Point(6, 88);
+            this.ifGreaterThanRadioButton.Name = "ifGreaterThanRadioButton";
+            this.ifGreaterThanRadioButton.Size = new System.Drawing.Size(131, 17);
+            this.ifGreaterThanRadioButton.TabIndex = 2;
+            this.ifGreaterThanRadioButton.Text = "If Greater Than (Keep)";
+            this.ifGreaterThanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ifLessThanRadioButton
+            // 
+            this.ifLessThanRadioButton.AutoSize = true;
+            this.ifLessThanRadioButton.Location = new System.Drawing.Point(6, 111);
+            this.ifLessThanRadioButton.Name = "ifLessThanRadioButton";
+            this.ifLessThanRadioButton.Size = new System.Drawing.Size(118, 17);
+            this.ifLessThanRadioButton.TabIndex = 3;
+            this.ifLessThanRadioButton.Text = "If Less Than (Keep)";
+            this.ifLessThanRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ifChangeRadioButton
+            // 
+            this.ifChangeRadioButton.AutoSize = true;
+            this.ifChangeRadioButton.Location = new System.Drawing.Point(6, 134);
+            this.ifChangeRadioButton.Name = "ifChangeRadioButton";
+            this.ifChangeRadioButton.Size = new System.Drawing.Size(184, 17);
+            this.ifChangeRadioButton.TabIndex = 4;
+            this.ifChangeRadioButton.Text = "If Change (Unknown Value Keep)";
+            this.ifChangeRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // newValueTextBox
+            // 
+            this.newValueTextBox.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.newValueTextBox.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.newValueTextBox.Location = new System.Drawing.Point(6, 62);
+            this.newValueTextBox.Name = "newValueTextBox";
+            this.newValueTextBox.Size = new System.Drawing.Size(173, 20);
+            this.newValueTextBox.TabIndex = 9;
+            this.newValueTextBox.Text = "FF00FF00FF00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(306, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Total:";
+            // 
+            // totalTextBoxText
+            // 
+            this.totalTextBoxText.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.totalTextBoxText.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.totalTextBoxText.Location = new System.Drawing.Point(344, 31);
+            this.totalTextBoxText.Name = "totalTextBoxText";
+            this.totalTextBoxText.ReadOnly = true;
+            this.totalTextBoxText.Size = new System.Drawing.Size(75, 20);
+            this.totalTextBoxText.TabIndex = 20;
+            this.totalTextBoxText.Text = "0xC00000FF";
+            // 
             // Search
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(364, 335);
+            this.ClientSize = new System.Drawing.Size(556, 315);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -198,6 +309,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.resultGrid)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -216,5 +329,14 @@
         private System.Windows.Forms.TextBox searchRangeValueTextBox;
         private System.Windows.Forms.TextBox lengthRangeAddressTextBox;
         private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox newValueTextBox;
+        private System.Windows.Forms.RadioButton ifChangeRadioButton;
+        private System.Windows.Forms.RadioButton ifLessThanRadioButton;
+        private System.Windows.Forms.RadioButton ifGreaterThanRadioButton;
+        private System.Windows.Forms.RadioButton ifEqualsRadioButton;
+        private System.Windows.Forms.RadioButton defaultRadioButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox totalTextBoxText;
     }
 }
